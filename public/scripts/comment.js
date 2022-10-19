eb3633929ece64951084f539d8e1032bd36edd26
Brian Ntanga <>
2022-10-19 09:25:59 -0400

    fixed script loading

--- public/scripts/comment.js
+++ public/scripts/comment.js
@@ -6,3 +6,30 @@
  */
 
 
+function viewCommentsByFreet(fields) {
+    fetch(`/api/comments/${fields.parentId}`)
+        .then(showResponse)
+        .catch(showResponse);
+}
+
+function createComment(fields) {
+    console.log("this is parent ID", fields.parentId);
+
+    fetch(`/api/comments/${fields.parentId}`, {method: 'POST', body: JSON.stringify(fields), headers: {'Content-Type': 'application/json'}})
+        .then(showResponse)
+        .catch(showResponse);
+}
+
+function editComment(fields) {
+    console.log("entering editing");
+    console.log("this is fields, ", JSON.stringify(fields));
+    fetch(`/api/comments/${fields.id}`, {method: 'PUT', body: JSON.stringify(fields), headers: {'Content-Type': 'application/json'}})
+        .then(showResponse)
+        .catch(showResponse);
+}
+
+function deleteComment(fields) {
+    fetch(`/api/comments/${fields.id}`, {method: 'DELETE'})
+        .then(showResponse)
+        .catch(showResponse);
+}
